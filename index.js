d95699ef972556c686b7b39ccbf52504fff6b0b7
bob <bogdan4uk@gmail.com>
2023-02-10 19:30:32 +0200

    User model; add user

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars')
-const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const path = require('path');
 const mongoose = require('mongoose');
 const homeRoutes = require('./routes/home');
 const cartRoutes = require('./routes/cart');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
+const User = require('./models/user');
 
 const app = express();
 
@@ -23,9 +24,19 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 // set views folder
 app.set('views', 'views');
-// set css
+// set public folder
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
+
+app.use(async (req, res, next) => {
+    try {
+        const user = await User.findById('63e675561bf46b8bee1695a7');
+        req.user = user;
+        next();
+    } catch (err) {
+        console.log(err);
+    }
+});
 
 // use routes
 app.use('/', homeRoutes);
@@ -41,6 +52,18 @@ async function start() {
         await mongoose.connect(url, {
             useNewUrlParser: true
         });
+
+        const candidate = await User.findOne();
+        if (!candidate) {
+            const user = new User({
+                email: 'bogdan4uk@gmail.com',
+                name: 'Bohdan',
+                cart: {
+                    items: []
+                }
+            });
+            await user.save();
+        }
 
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
